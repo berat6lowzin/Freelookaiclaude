8145d4ea440728a70089c90b199d3873b7fadd53
berat6lowzin <bero4801@gmail.com>
2026-08-27 00:36:52 +0300

    Create FreelookMod.java

---
+++ src/main/java/com/example/freelook/FreelookMod.java
@@ -0,0 +1,12 @@
+package com.example.freelook;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FreelookMod implements ModInitializer {
+    public static final String MOD_ID = "freelook";
+
+    @Override
+    public void onInitialize() {
+        // Client-only mod; all the logic lives in FreelookClient.
+    }
+}
